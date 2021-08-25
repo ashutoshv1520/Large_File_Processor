0ae3d633f4f19b6c27de96ac92a589d16fe808c5
Ashutosh Verma <ashutoshv1520@gmail.com>
2021-08-25 19:27:51 +0530

    Update large_file_processor.py

--- large_file_processor.py
+++ large_file_processor.py
@@ -44,7 +44,7 @@ class Processor:
             cursor.execute('DROP TABLE IF EXISTS '+self.db_name+'.'+self.table_name+';')
             cursor.execute('CREATE TABLE '+self.db_name+'.'+self.table_name+'(name varchar(255),sku varchar(255),description varchar(255));')
             print("Table Created.")
-            load_sql = "LOAD DATA LOCAL INFILE 'data/"+file_name+"' INTO TABLE product.product_data\
+            load_sql = "LOAD DATA LOCAL INFILE 'data/"+file_name+"' INTO TABLE "+self.db_name+'.'+self.table_name+"\
  FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';"
             cursor.execute(load_sql)
             cursor.execute("DELETE FROM "+self.db_name+"."+self.table_name+" WHERE name='name' and sku='sku' and description='description';")
@@ -80,29 +80,43 @@ class Processor:
         except Exception as e:
             print('Error: {}'.format(str(e)))
             sys.exit(1)
+    
+    def update_table_from_csv(self, file_name="products.csv"):
+        '''
+        This function load a csv file and update data to MySQL table.
+        '''
+        try:
+            cursor = self.con.cursor()
+            load_sql = "LOAD DATA LOCAL INFILE 'data/"+file_name+"' INTO TABLE "+self.db_name+'.'+self.table_name+"\
+ FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';"
+            cursor.execute(load_sql)
+            cursor.execute("DELETE FROM "+self.db_name+"."+self.table_name+" WHERE name='name' and sku='sku' and description='description';")
+            print('Succuessfully updated the table from csv.')
+        except Exception as e:
+            print('Error: {}'.format(str(e)))
+            sys.exit(1)
         
-
 
 
 def main():
     obj=Processor()
-    flag=0
-    while(flag==0):
+    flag=1
+    while(flag==1):
         print("Select appropriate option:")
         print('''1. insert_csv_data (table,data-feeding)
 2. insert data on basis of sku
 3. update data on basis of sku 
-4. create aggregated table 
-4. delete data 
+4. create aggregated table  
 5. update data 
 6. connect to database 
 7. disconnect.''')
         print("press 0 if you wish to exit\n")
         inp=int(input())
         if inp==0:
-            flag=1
+            flag=0
         elif inp==1:
-            obj.insert_csv_data()
+            file=input("Enter file name: ")
+            obj.create_table_from_csv(file)
         elif inp==2:
             name=input("Enter name: ")
             sku=input("Enter sku: ")
@@ -116,10 +130,16 @@ def main():
         elif inp==4:
             agg_table_name=input("Enter new table name: ")
             obj.create_aggregated_table(agg_table_name)
+        elif inp==5:
+            file=input("Enter file name: ")
+            obj.update_table_from_csv(file)
         elif inp==6:
             obj.connect()
         elif inp==7:
             obj.disconnect()
+        print("Do you wish to continue? 0/1")
+        flag=int(input())
+
         
 
 if __name__ == "__main__":
